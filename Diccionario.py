7bf839c696e82ce8e4a217b8bc75e425ac85a6ce
ZahidMTZ <79343913+ZahidMTZ@users.noreply.github.com>
2023-09-12 19:03:27 -0600

    Add files via upload

---
+++ Diccionario.py
@@ -0,0 +1,12 @@
+
+Alumnos={
+2054603: "Danyelin",
+2022069: "Edwin",
+1843610: "Zahid",
+1817307: "Eric",
+1966947: "Damian"
+}
+
+for x,j  in Alumnos.items():
+    print("Matricula: ",x," Nombre: ",j)
+print(type(Alumnos))
